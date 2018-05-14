44f0b4b56c273fce10d6e6d95a9b8fa5667b099e
Morteza88 <morteza.esmailpoor@gmail.com>
2018-05-14 19:22:41 +0400

    add SmtpClientModel

---
+++ EventsReminder/Models/SmtpClientModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsReminder.Models
+{
+    public class SmtpClientModel
+    {
+        public int Id { get; set; }
+        public string Host { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool EmailSentSuccessful { get; set; }
+
+        public SmtpClientModel(string host, string email, string password, bool emailSentSuccessful)
+        {
+            Host = host;
+            Email = email;
+            Password = password;
+            EmailSentSuccessful = emailSentSuccessful;
+        }
+    }
+}
