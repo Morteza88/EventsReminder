134b675a72b15860014e98d32f8bfa6616fc2b12
Morteza88 <morteza.esmailpoor@gmail.com>
2018-05-15 13:51:07 +0400

    change App StartupUri

--- EventsReminder/View/EventsReminderWindow.xaml.cs
+++ EventsReminder/View/EventsReminderWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace EventsReminder.View
 
             EventDB.init();
             IEnumerable<EventModel> eventModels = EventDB.selectAll();
-            Console.WriteLine("Events loaded from LiteDB");
+            Console.WriteLine("Events (Count="+ eventModels.ToList().Count + ") loaded from LiteDB");
 
             this.EventsReminderView = new EventsReminderViewModel(eventModels);
         }
